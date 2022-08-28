9abb506f42a8ced4036bf222cb9c3534e24867b8
TsungJu <e285171@gmail.com>
2022-08-28 23:19:49 +0800

    Update SWAGGER_HOST

--- app/config/config.py
+++ app/config/config.py
@@ -12,7 +12,7 @@ class DevelopmentConfigDocker:
 
 class ProductionConfig:
     DATABASE_URL = 'mongodb+srv://leolee:su3cp3gj94@cluster0.slk3i.mongodb.net/app_database?retryWrites=true&w=majority'
-    SWAGGER_HOST = 'leonardapi.herokuapp.com'
+    SWAGGER_HOST = 'data-analyze-ml-toolset-backend.azurewebsites.net'
     REDISDB_URL = os.environ['REDIS_URL']
 
 config = {
