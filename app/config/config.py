c579c3f93fb14360cbe7ab8deeeda410e5919d8d
TsungJu <e285171@gmail.com>
2022-10-27 15:01:35 +0800

    Bug Fix : prod env use persistent shared storage

--- app/config/config.py
+++ app/config/config.py
@@ -16,7 +16,7 @@ class ProductionConfig:
     DATABASE_URL = 'mongodb+srv://leolee:su3cp3gj94@cluster0.slk3i.mongodb.net/app_database?retryWrites=true&w=majority'
     SWAGGER_HOST = 'data-analyze-ml-toolset-backend.azurewebsites.net'
     REDISDB_URL = os.environ['REDIS_URL']
-    UPLOAD_FOLDER = '/home/app/data/'
+    UPLOAD_FOLDER = 'app/data/'
 
 config = {
     'development' : DevelopmentConfig,
