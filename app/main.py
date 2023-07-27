92b980cf3d257b51ef8da3a6e3e85063889891da
TsungJu <e285171@gmail.com>
2023-07-27 14:37:44 +0800

    Bug Fix: remove predicting

--- app/main.py
+++ app/main.py
@@ -3,13 +3,13 @@ from .auth.auth import auth_blueprint
 from .file.file import file_blueprint
 from .analyzing.analyzing import analyzing_blueprint
 from .modeling.modeling import modeling_blueprint
-from .predicting.predicting import predicting_blueprint
+#from .predicting.predicting import predicting_blueprint
 
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(file_blueprint)
 app.register_blueprint(analyzing_blueprint)
 app.register_blueprint(modeling_blueprint)
-app.register_blueprint(predicting_blueprint)
+#app.register_blueprint(predicting_blueprint)
 
 @app.cli.command()
 def test():
